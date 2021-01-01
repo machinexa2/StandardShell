72fe5fe3d0a28da260fab5cf5fddf2b8db640645
machinexa2 <machinexa>
2021-01-02 00:11:37 +0545

    12:11:37 AM 01/02/2021

---
+++ README.md
@@ -0,0 +1,21 @@
+# Standardshell
+Build a standard shell
+
+## Working
+* Shell is built by stdin and stdout. Take stdin and give stdout. This module is useful during RCE.
+```python
+from standardshell import LinuxShell
+```
+* To develop a shell define stdout yourself. stdin is predefined as keyboard input.
+```python
+def rce_code(self, cmd):
+	""" Write RCE Code to send command and return cmd output """
+	return output
+LinuxShell.stdout = rce_code #pwd should return /home/username
+```
+* Boom, You got a shell  
+```python
+rootshell = LinuxShell('root')
+rootshell.interact()
+root@machine:/root#
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from setuptools import setup
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name="standard-shell",
+    version="1.0.0",
+    author="Machine Yadav",
+    author_email="machinexa@gmail.com",
+    description="Module for building shells",
+    long_description=next(line.rstrip('\n') for line in open("README.md") if line),
+    long_description_content_type="text/markdown",
+    url="https://github.com/machinexa2/StandardShell",
+    packages=["standardshell"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "interruptingcow"
+    ],
+)
